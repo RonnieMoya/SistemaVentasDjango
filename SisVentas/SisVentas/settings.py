69e487eb70bf08dabd23f2a780be6a8d47250168
Ronnie Moya <ronnie.moya@inacapmail.cl>
2025-03-30 21:59:31 -0300

    Agregando plantilla adminLte y carpeta static

--- SisVentas/SisVentas/settings.py
+++ SisVentas/SisVentas/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import SisVentas.db as db
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'SisVentas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,6 +114,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR / "static"),
+    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- SisVentas/sVentas/views.py
+++ SisVentas/sVentas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 
+from sVentas.models import Category
 # Create your views here.
+def myfirstView(request):
+    data={
+        'name': 'Ronnie',
+        'categorias': Category.objects.all()
+    }
+    return render (request, "home.html",data)
